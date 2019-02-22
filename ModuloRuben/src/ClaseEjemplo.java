244ba2f2801505b823743bef53167568ae99776b
al036570 <al036570@pc2201>
2019-02-22 15:39:48 +0100

    Nombres modificados y bucle añadido a la 'ClaseEjemplo'.

---
+++ ModuloRuben/src/ClaseEjemplo.java
@@ -0,0 +1,21 @@
+/**
+ * @author  Rubén López Vega y Daniel VillarrealPeña
+ * @since  22/02/2019
+ */
+
+public class ClaseEjemplo {
+
+    /**
+     * Modulo agregado al proyecto de Daniel.
+     *
+     */
+
+    public static void main(String[] args) {
+
+    	for (int i = 0; i < 5; i++) {
+            System.out.println("Hello world");
+    	}
+        // Comentario alea
+
+    }
+}
