8bea4f4a11d2a97bcb0b34fe80629d7ff04833a0
ErreVega <errevega1991@gmail.com>
2019-02-22 15:33:47 +0100

    start

--- ModuloRuben/src/ClaseRuben.java
+++ ModuloRuben/src/ClaseRuben.java
@@ -1,2 +1,18 @@
+/**
+ * @author Rubbén López Vega
+ * @since 22/02/2019
+ */
 public class ClaseRuben {
+
+    /**
+     * Modulo agregado al proyecto de Daniel.
+     *
+     */
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world");
+        // comentarios
+
+    }
 }
