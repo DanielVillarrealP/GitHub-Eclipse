f68c6eee5e4a1f3cdb51672b5d843207ddbffbca
al036570 <al036570@pc2201>
2019-02-22 15:40:39 +0100

    Comentario corregido.

--- ModuloRuben/src/ClaseEjemplo.java
+++ ModuloRuben/src/ClaseEjemplo.java
@@ -15,7 +15,7 @@ public class ClaseEjemplo {
     	for (int i = 0; i < 5; i++) {
             System.out.println("Hello world");
     	}
-        // Comentario alea
+        // Comentario aleatorio
 
     }
 }
